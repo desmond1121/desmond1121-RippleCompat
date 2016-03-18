65ac5eb978dee7fd2d4221166d3a666b015952d5
jonatan <jonatansalas@live.com.ar>
2016-03-17 21:44:23 -0300

    Added license detail to all files Added contributor details to classes

--- ripplecompat/src/main/java/com/desmond/ripple/view/RippleCompat.java
+++ ripplecompat/src/main/java/com/desmond/ripple/view/RippleCompat.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Jonatan Salas
+ * Copyright (C) 2015 Desmond Yao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.desmond.ripple.view;
 
 import android.annotation.TargetApi;
@@ -23,6 +39,10 @@ import com.desmond.ripple.config.RippleConfig;
 import com.desmond.ripple.drawable.RippleCompatDrawable;
 import com.desmond.ripple.util.RippleUtil;
 
+/**
+ * @author Desmond Yao
+ * @author Jonatan Salas
+ */
 public class RippleCompat {
     private static final String TAG = RippleCompat.class.getSimpleName();
     private static InputMethodManager manager = null;
